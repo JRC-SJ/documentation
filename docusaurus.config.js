9f8e6db7470db13c507230683bd46084a1b89fcc
nana-ike <123707191+nana-ike@users.noreply.github.com>
2024-09-11 15:06:08 +0900

    Update docusaurus.config.js
    upload test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Observeasy Documentation',
+  title: 'Observeasy Documentation Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.png',
 
