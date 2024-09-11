96e275547bcf6c4262d370b9c45b26b5f2efb964
nana-ike <123707191+nana-ike@users.noreply.github.com>
2024-09-11 14:53:07 +0900

    changed favicon
    changed favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Observeasy Documentation',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Set the production url of your site here
   url: 'https://jrc-sj.github.io/',
